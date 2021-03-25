f18f469cd563056bdfc177455e31daf3f844188d
CovDev <shusten@hotmail.com>
2021-03-25 14:47:16 -0300

    :sparkles: feat: Lista de tarefas

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Task } from '../model/task.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EMPTY, Observable, Subject } from 'rxjs';
+import { catchError, switchMap, take } from 'rxjs/operators';
+import { Task } from '../model/task';
+import { AlertModalService } from '../shared/alert-modal.service';
+import { TasksService } from '../tasks.service';
+
 
 @Component({
   selector: 'app-list',
@@ -8,21 +14,70 @@ import { Task } from '../model/task.model';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+        private     service:    TasksService,
+        private      router:    Router,
+        private       route:    ActivatedRoute,
+        private alertService:   AlertModalService,
 
-  public taskList : Task [] =[];
+  ) { }
 
+  selectedTask: Task;
+
+  //public taskList : Task [] =[];
+  tasks$: Observable<Task[]>;
+  error$ = new Subject<boolean>();
+
+  
   ngOnInit() {
+    this.tasks$ = this.service.list().pipe(
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return EMPTY;
+      })
+    )
   }
 
-  addTask(){
-    this.taskList.push(new Task());
+  onEdit(id) {
+    this.router.navigate(['edit', id], { relativeTo: this.route });
   }
 
-  removeTask(index:number){
-    if(index > -1){
-      this.taskList.splice(index,1);
+
+  onCloseTask(task) {
+    this.selectedTask = task;
+
+    const result$ = this.alertService.showConfirm('Confirmação', 'Tem certeza que deseja encerrar está tarefa?');
+    result$.asObservable()
+    .pipe(
+      take(1),
+      switchMap(result => result ? this.service.remove(task.id) : EMPTY)
+    )
+    .subscribe(
+      success => {
+        this.alertService.showAlertSuccess('A tarefa foi encerrada.')
+        this.onRefresh();
+      },
+      error => { 
+        this.alertService.showAlertDanger('Erro ao encerrar tarefa. Tente mais tarde.')
     }
-
+    )
   }
+
+
+  onRefresh() {
+    this.tasks$ = this.service.list()
+    .pipe(
+      catchError(error => {
+        console.error(error);
+        this.handleError();
+        return EMPTY;
+      })
+    ); 
+    }
+    
+
+    handleError() {
+      this.alertService.showAlertDanger('Erro ao carregar as tarefas. Tente mais tarde.');
+    }
 }
