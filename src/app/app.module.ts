e2bcf57755271d4089fe206bb240e8dab20c5e27
CovDev <shusten@hotmail.com>
2021-03-25 13:51:41 -0300

    :heavy_plus_sign: Declaraçoes do modulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {  ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './/app-routing.module';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    TaskFormComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot(),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
